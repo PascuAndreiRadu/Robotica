6b9fc87c5219b8dbce2a51748db65a933453b521
andonecatalin <radua.pascu@gmail.com>
2026-04-20 14:47:44 +0200

    Final Push

--- build/topics_pkg/catkin_generated/setup_cached.sh
+++ build/topics_pkg/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/vm/catkin_ws/devel/.private/topics_pkg:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/vm/catkin_ws/devel/.private/topics_pkg/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/vm/catkin_ws/devel/.private/topics_pkg/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/vm/catkin_ws/build/topics_pkg'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/vm/catkin_ws/devel/.private/topics_pkg/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/vm/catkin_ws/src/topics_pkg:$ROS_PACKAGE_PATH"
